fc36670d511968f287c01ec914b74d2f1a69e9dd
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2021-03-29 13:39:30 +0200

    finished after dinner

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogHomeComponent } from './components/blog-home/blog-home.component';
+import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
@@ -14,9 +16,12 @@ path: 'contact',
 component: ContactComponent,
 },
 {
-path: 'blog'
-,
-component: BlogComponent,
+path: 'blog',
+component: BlogHomeComponent,
+},
+{
+path: 'blog/detail/:id',
+component: BlogItemDetailsComponent
 }];
 
 @NgModule({
